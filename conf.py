d7309e7e09ae3e0c2f861623013e36129c94c2fb
Tetsuo Koyama <tkoyama010@gmail.com>
2021-05-27 21:32:51 +0900

    📝 Create Read the Docs page (#2)
    * 📝 Create Read the Docs page
    
    * Delete .readthedocs.yaml
    
    * Create environment.yml
    
    * Create requirements.txt
    
    * Delete requirements.txt
    
    * Update environment.yml
    
    * Update environment.yml
    
    * Create .readthedocs.yaml
    
    * Update environment.yml
    
    * Update environment.yml
    
    * Update environment.yml
    
    * Update conf.py

---
+++ conf.py
@@ -0,0 +1,31 @@
+# BASEDIR is set by <lang>/conf.py
+"""
+Use "-D language=<LANG>" option to build a localized CoFEA document.
+For example::
+
+    sphinx-build -D language=ja -b html . _build/html
+
+This conf.py do:
+
+- Specify `locale_dirs` and `gettext_compact`.
+- Overrides source directory as 'CoFEA/docs/`.
+
+"""
+import os
+import shutil
+import sys
+
+from sphinx.util.pycompat import execfile_
+
+os.system("git submodule update --init --force --recursive cadquery")
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+
+execfile_(os.path.join(BASEDIR, "cadquery/doc/conf.py"), globals())
+
+locale_dirs = [os.path.join(BASEDIR, "locale/")]
+
+
+def setup(app):
+    app.srcdir = os.path.join(BASEDIR, "cadquery/doc/")
+    app.confdir = app.srcdir

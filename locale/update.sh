d8ba3234cda5ddb31eef8eb7f02523de80b4840f
Tetsuo Koyama <tkoyama010@gmail.com>
2021-05-27 19:04:21 +0900

    👷 Update locale/update.sh

--- locale/update.sh
+++ locale/update.sh
@@ -10,11 +10,9 @@ LOCAL_PYTHON_PATH=$CONDA
 
 # pull po files from transifex
 cd `dirname $0`
-rm -Rf benchmarks
-cp -r ../CoFEA/docs/benchmarks .
 $LOCAL_PYTHON_PATH/sphinx-intl create-transifexrc
 #rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
-$LOCAL_PYTHON_PATH/sphinx-build -T -b gettext ../CoFEA/docs pot
+$LOCAL_PYTHON_PATH/sphinx-build -T -b gettext ../cadquery/doc pot
 $LOCAL_PYTHON_PATH/sphinx-intl update-txconfig-resources -p pot -d .
 cat .tx/config
 $LOCAL_PYTHON_PATH/tx push -s --skip
